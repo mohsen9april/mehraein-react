7e794fd2afdb7627a0dc2fa6ca4a5af208d1013f
Mohsen9april <Mohsen9april@gmail.com>
2022-07-07 16:42:07 +0430

    add product section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import Header from './components/Header'
 import ImageSlider from './components/ImageSlider';
+import Products from './components/Products';
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <Header/>
       <ImageSlider/>
+      <Products/>
     </>
   )
 }
